4bcce245bd1216f3e4a50960359e98fe35ca35fe
mohamedsaeed59 <mohamedsaeed5965@gmail.com>
2023-12-09 18:51:25 +0200

    make change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Sidebar } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import "./App.css";
+import { useStateContext } from "./contexts/ContextProvider";
 
 function App() {
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
   return (
     <div>
       <BrowserRouter>
@@ -42,22 +44,22 @@ function App() {
             <Routes>
               <Route path="/" element={(<Ecommerce />)} />
               <Route path="/ecommerce" element={(<Ecommerce />)} />
-              <Route path="orders" element={<Orders />} />
-              <Route path="employees" element={<Employees />} />
-              <Route path="customers" element={<Customers />} />
-              <Route path="kanban" element={<Kanban />} />
-              <Route path="editor" element={<Editor />} />
-              <Route path="calendar" element={<Calendar />} />
-              <Route path="color-picker" element={<ColorPicker />} />
-              <Route path="line" element={<Line />} />
-              <Route path="area" element={<Area />} />
-              <Route path="bar" element={<Bar />} />
-              <Route path="pie" element={<Pie />} /> 
-              <Route path="financial" element={<Financial />} />
-              <Route path="color-mapping" element={<ColorMapping />} />
-              <Route path="pyramid" element={<Pyramid />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} /> 
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
               <Route path="/stacked" element={<Stacked />} />
-            </Routes>
+            </Routes>/
           </div>
         </div>
       </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import App from './App';
 import "./index.css";
+import {ContextProvider} from './contexts/ContextProvider';
 
 const root = ReactDOM.unstable_createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <ContextProvider>
+    <App /> 
+  </ContextProvider>
+); 
